4ed0c542653a179e730b5ad033dbc4bb529a12b0
David Marques <davidmarques99@hotmail.com>
2024-07-23 19:51:41 -0300

    feat: created index method on service interface

--- app-laravel/app/Interfaces/Api/ISupplierService.php
+++ app-laravel/app/Interfaces/Api/ISupplierService.php
@@ -6,6 +6,7 @@ use App\DTOS\Api\SupplierDTO;
 use stdClass;
 
 interface ISupplierService {
+    public function index();
     public function store(SupplierDTO $data): stdClass;
     public function show(string $id): stdClass | null;
     public function update(string $id, SupplierDTO $data): stdClass | null;
