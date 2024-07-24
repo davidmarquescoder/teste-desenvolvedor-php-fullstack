154510fd559d06096dda7dc80b208955382e9ccf
David Marques <davidmarques99@hotmail.com>
2024-07-24 15:02:13 -0300

    feat: created request service to return all suppliers.

---
+++ app-vue3/src/services/suppliers/getSuppliers.js
@@ -0,0 +1,19 @@
+// import router from '@/router'
+import axios from 'axios'
+
+export default async function getSuppliers(page = 1) {
+  return await axios
+    .get(`${import.meta.env.VITE_API_ENDPOINT_SUPPLIERS}?page=${page}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      withCredentials: false
+    })
+    .then((response) => {
+      return response.data
+    })
+    .catch((error) => {
+      console.log(error.response.data.message)
+    })
+}
