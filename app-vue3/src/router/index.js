3af391736684a1830b6f466a80512b348676f454
David Marques <davidmarques99@hotmail.com>
2024-07-24 15:01:10 -0300

    feat: created new routes.

--- app-vue3/src/router/index.js
+++ app-vue3/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import IndexView from '../views/IndexView.vue'
+import RegisterSupplier from '../views/RegisterSupplier.vue'
+import EditSupplier from '../views/EditSupplier.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'index',
       component: IndexView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterSupplier
+    },
+    {
+      path: '/edit',
+      name: 'edit',
+      component: EditSupplier
     }
   ]
 })
