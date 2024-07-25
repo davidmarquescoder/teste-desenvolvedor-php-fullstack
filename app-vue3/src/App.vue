4cb12a0edba456c189ee107f3af0e6e4a5d8926c
David Marques <davidmarques99@hotmail.com>
2024-07-25 18:50:33 -0300

    feat: calling message component on App.

--- app-vue3/src/App.vue
+++ app-vue3/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
     <Navbar />
+    <Messages
+      :v-if="$store.state.response_messages"
+      :message="$store.state.response_messages"
+    />
   </div>
   <RouterView />
 </template>
 
 <script setup>
 import Navbar from './components/Navbar.vue'
+import Messages from './components/Messages.vue'
 </script>
 
 <style scoped></style>
